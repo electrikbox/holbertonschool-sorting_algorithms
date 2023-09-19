cb1dffb52064a63652e364fee8290aaabb9d3c43
lucieruin <lucie.wattez@hotmail.fr>
2023-09-19 10:45:01 +0200

    quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort - orts an array of integers in ascending order
+ * @array: pointer to the array
+ * @size: the size of the array
+ *
+*/
+
+void quick_sort(int *array, size_t size)
+{
+
+}
